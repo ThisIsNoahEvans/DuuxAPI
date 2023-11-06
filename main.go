4692d19c0eb6bfa88ced270c70ae764a87174f15
Noah Evans <noah@itsnoahevans.co.uk>
2023-11-06 18:09:06 +0000

    Get auth token using code that was emailed

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"encoding/json"
 )
 
-func login() {
+func sendLoginCode() {
 	fmt.Println("Logging in...")
 
 	// ask for email
@@ -56,7 +56,7 @@ func login() {
 		fmt.Println(err)
 		return
 	}
-	
+
 	loginCodeResponse := LoginCodeResponse{}
 	json.Unmarshal(body, &loginCodeResponse)
 
@@ -68,12 +68,76 @@ func login() {
 		fmt.Scanln(&loginCode)
 
 		// pass login code to getAuthToken()
+		getAuthToken(loginCode)
 	} else {
 		fmt.Println("Failed!")
 		fmt.Println("Message: " + loginCodeResponse.Message)
 	}
 }
 
+func getAuthToken(loginCode string) {
+	fmt.Println("Getting auth token...")
+
+	url := "https://v4.api.cloudgarden.nl/auth/oauth2/token"
+	method := "POST"
+
+	// Parameters from the iOS app.
+	// Effectively, we are emulating the iOS app here.
+	payload := strings.NewReader(`{
+    "grant_type": "authorization_code",
+    "code": "` + loginCode + `",
+    "code_verifier": "j2yOyLB3KbdFE3ZjYtm6QGMDBL8-_5FWo0UMYkRVdljLYATNMFa4fJ86vwe3jsVHPsuZcZXGLkezJqHnvhLrRMJjymjDnw-LvCA8WVAFQZNWwFmiUULgNsldc29ZyI36",
+    "client_id": "83f34a5fa5faca9023c78980a57a87b41f6972fc4ee45e9c",
+    "redirect_uri": "https://duux-deeplink.vercel.app/login/verify",
+    "makeAccessTokenLongLasting": true
+}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Host", "v4.api.cloudgarden.nl")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("User-Agent", "Duux/154 CFNetwork/1335.0.3.4 Darwin/21.6.0")
+	req.Header.Add("Accept-Language", "en-GB,en;q=0.9")
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	authTokenResponse := AuthTokenResponse{}
+	json.Unmarshal(body, &authTokenResponse)
+
+	if authTokenResponse.AccessToken != "" {
+		fmt.Println("Successfully got auth token!")
+		// convert expiresIn to string
+		expiresIn := string(rune(authTokenResponse.ExpiresIn))
+
+		fmt.Println("Access token: " + authTokenResponse.AccessToken)
+		fmt.Println("Refresh token: " + authTokenResponse.RefreshToken)
+		fmt.Println("Expires in: " + expiresIn)
+		fmt.Println("Token type: " + authTokenResponse.TokenType)
+	} else {
+		fmt.Println("Failed!")
+		fmt.Println("Message: " + authTokenResponse.AccessToken)
+	}
+
+}
+
 func main() {
-	login()
+	sendLoginCode()
 }
--- structs.go
+++ structs.go
@@ -1,8 +1,15 @@
 package main
 
 // Struct for LoginCodeResponse
-
 type LoginCodeResponse struct {
 	Success bool `json:"success"`
 	Message string `json:"message"`
+}
+
+// Struct for AuthTokenResponse
+type AuthTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn int `json:"expires_in"`
+	TokenType string `json:"token_type"`
 }
